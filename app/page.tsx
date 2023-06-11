baaa558742563bdad3e0101fc03a763b2d4b85e6
Vishal <vismbs@outlook.com>
2023-06-11 20:34:00 +0530

    Initial

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,68 @@
-import Image from 'next/image'
+"use client";
+
+import { Montserrat } from "next/font/google";
+import { Configuration, OpenAIApi } from "openai";
+import { useState } from "react";
+
+const montserrat = Montserrat({ subsets: ["latin"] });
+const exampleName = [
+  "Keanu Reeves",
+  "Lana Del Ray",
+  "Harry Styles",
+  "Joseph Vijay",
+  "Ajith Kumar",
+  "Keanu Reeves",
+  "Lana Del Ray",
+  "Harry Styles",
+  "Joseph Vijay",
+  "Ajith Kumar",
+];
+
+const exName = exampleName[Math.floor(Math.random() * 10)];
+
+async function handleClick(usrPrompt: string) {
+  const openConfig = new Configuration({
+    apiKey: process.env.OPNEAI_KEY,
+  });
+
+  const openAI = new OpenAIApi(openConfig);
+  const chatCompletion = await openAI.createCompletion({
+    model: "text-davinci-003",
+    prompt: `Assume that you are a person with a certain celebrity's name in you mind and you want the user to guess it and upon correct guess you reveal the Celebrity's name. The user will ask questions about that celebrity and you should give hints to them. don't reveal the celebrity's name unless the user guess is right. When the user guesses it will be in this format [answer] .Celebrity's name: ${exName}; User Prompt: ${usrPrompt} ?`,
+    max_tokens: 1000,
+  });
+
+  return chatCompletion.data.choices[0].text;
+}
 
 export default function Home() {
+  const [compText, setCompText] = useState("");
+  const [usrPromp, setUserPrompt] = useState("");
+  console.log(compText);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="h-screen">
+      <div className="flex flex-col justify-center items-center h-full">
+        <div>
+          <input
+            type="text"
+            placeholder="Ask for Hints!!"
+            value={usrPromp}
+            onChange={(eV) => setUserPrompt(eV.target.value)}
+            className={`p-3 w-[500px] rounded-xl outline-none text-xs ${montserrat.className} m-2 w-1/2`}
+          />
+          <button
+            className={`p-3 bg-[#F7E6C4] rounded-xl text-[#606C5D] ${montserrat.className} text-xs`}
+            onClick={async () => {
+              setCompText((await handleClick(usrPromp)) as string);
+            }}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            Submit
+          </button>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <p className={`${montserrat.className} text-sm text-[#F7E6C4]`}>
+          {compText}
+        </p>
       </div>
     </main>
-  )
+  );
 }
